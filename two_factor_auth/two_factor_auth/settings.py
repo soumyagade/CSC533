f4809f06f791af30891c8bc8e4ca4cb70a957381
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-11-09 20:23:02 -0500

    Bugfix
    I forgot to add quotes in my previous commit.

--- two_factor_auth/two_factor_auth/settings.py
+++ two_factor_auth/two_factor_auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=%_oi$s6_-%@77e0k7%bhlqh-xtfv^sk+e_#x*s*xzq@@=ph9p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
